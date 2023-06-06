0ff17283703ddd8f9d38840a555dab2705d2e874
Antonio <netoifpe@gmail.com>
2023-06-06 09:04:59 -0300

    fix: button footer

--- src/Components/Navbar/NavDefault/index.tsx
+++ src/Components/Navbar/NavDefault/index.tsx
@@ -9,12 +9,14 @@ export const NavDefault = ({ style }: InavDefaultProps) => {
     <nav className={style}>
       <Link
         href={"/"}
-        className="flex h-12  w-auto  items-center justify-center  rounded  px-7 py-3 text-base font-semibold text-grey0 hover:bg-grey8">
+        className="flex h-12  w-auto  items-center justify-center  rounded  px-7 py-3 text-base font-semibold text-grey0 hover:bg-grey8"
+        id="link-login-page">
         Fazer Login
       </Link>
       <Link
         href={"/"}
-        className="flex h-12 w-auto items-center justify-center  rounded border-[1.5px] border-grey4 px-7 py-3 text-base font-semibold text-grey0 hover:border-grey1 hover:bg-grey1 hover:text-grey10">
+        className="flex h-12 w-auto items-center justify-center  rounded border-[1.5px] border-grey4 px-7 py-3 text-base font-semibold text-grey0 hover:border-grey1 hover:bg-grey1 hover:text-grey10"
+        id="link-register-page">
         Cadastrar
       </Link>
     </nav>
